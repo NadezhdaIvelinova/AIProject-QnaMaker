1194bb69a0a8e7eefba6fe1de046d321982f5990
nadezhdaivanova589@gmail.com <nadezhdaivanova589@gmail.com>
2020-02-02 15:45:45 +0200

    Initial version of qnamaker with knowledge base and bot - added follow-up prompts

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=aiprojectqnaknowledgebase-bot-Web-Deploy.pubxml -p:Password=u25qpNF2uLSlWispdaNDPngPRYLdWdfclsxanLquiKqyj2iKTjDNorvZoDA0
+
